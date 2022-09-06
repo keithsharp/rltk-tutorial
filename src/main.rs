1015fa35fe19dbeb7560244686a0204fc1ae02ea
Keith Sharp <kms@passback.co.uk>
2022-09-06 19:30:11 +0100

    Make the red glyphs move using a system.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use specs::prelude::*;
 use specs_derive::Component;
 use std::cmp::{max, min};
 
+// Components
 #[derive(Component)]
 struct Position {
     x: i32,
@@ -16,12 +17,34 @@ struct Renderable {
     bg: rltk::RGB,
 }
 
+#[derive(Component)]
+struct LeftMover {}
+
+// Systems
+struct LeftWalker {}
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, data: Self::SystemData) {
+        let (lefty, mut pos) = data;
+        for (_, mut pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 {
+                pos.x = 79;
+            }
+        }
+    }
+}
+
+// Game State
 struct State {
     ecs: World,
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
+        self.run_systems();
+
         ctx.cls();
 
         let positions = self.ecs.read_storage::<Position>();
@@ -33,6 +56,15 @@ impl GameState for State {
     }
 }
 
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker {};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
+
+// Main Function
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
@@ -42,6 +74,7 @@ fn main() -> rltk::BError {
     let mut gs = State { ecs: World::new() };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
 
     gs.ecs
         .create_entity()
@@ -62,6 +95,7 @@ fn main() -> rltk::BError {
                 fg: RGB::named(rltk::RED),
                 bg: RGB::named(rltk::BLACK),
             })
+            .with(LeftMover {})
             .build();
     }
 
