d3b4e64f92fa9dd1f72edd0ed279f4a2175f920e
Keith Sharp <kms@passback.co.uk>
2022-09-07 17:01:10 +0100

    Move player with Numpad and VI keys.

--- src/player.rs
+++ src/player.rs
@@ -23,10 +23,18 @@ pub fn try_move_player(dx: i32, dy: i32, ecs: &mut World) {
 pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
     if let Some(key) = ctx.key {
         match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            VirtualKeyCode::Left | VirtualKeyCode::Numpad4 | VirtualKeyCode::H => {
+                try_move_player(-1, 0, &mut gs.ecs)
+            }
+            VirtualKeyCode::Right | VirtualKeyCode::Numpad6 | VirtualKeyCode::L => {
+                try_move_player(1, 0, &mut gs.ecs)
+            }
+            VirtualKeyCode::Up | VirtualKeyCode::Numpad8 | VirtualKeyCode::K => {
+                try_move_player(0, -1, &mut gs.ecs)
+            }
+            VirtualKeyCode::Down | VirtualKeyCode::Numpad2 | VirtualKeyCode::J => {
+                try_move_player(0, 1, &mut gs.ecs)
+            }
             _ => {}
         }
     }
