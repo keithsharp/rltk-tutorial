6690ca8854869f7d8dce86068f5cf3b2abfc2312
Keith Sharp <kms@passback.co.uk>
2022-09-10 15:58:51 +0100

    Monsters will chase players.

--- src/monster_ai_system.rs
+++ src/monster_ai_system.rs
@@ -1,26 +1,42 @@
 use rltk::{console, Point};
 use specs::prelude::*;
 
+use crate::Map;
 use crate::Monster;
 use crate::Name;
+use crate::Position;
 use crate::Viewshed;
 
 pub struct MonsterAiSystem {}
 
 impl<'a> System<'a> for MonsterAiSystem {
     type SystemData = (
+        ReadExpect<'a, Map>,
         ReadExpect<'a, Point>,
-        ReadStorage<'a, Viewshed>,
+        WriteStorage<'a, Viewshed>,
         ReadStorage<'a, Monster>,
         ReadStorage<'a, Name>,
+        WriteStorage<'a, Position>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (pos, viewshed, monster, name) = data;
+        let (map, player_pos, mut viewshed, monster, name, mut position) = data;
 
-        for (viewshed, _, name) in (&viewshed, &monster, &name).join() {
-            if viewshed.visible_tiles.contains(&*pos) {
+        for (mut viewshed, _, name, mut pos) in
+            (&mut viewshed, &monster, &name, &mut position).join()
+        {
+            if viewshed.visible_tiles.contains(&*player_pos) {
                 console::log(&format!("{} shouts insults", name.name));
+                let path = rltk::a_star_search(
+                    map.xy_to_idx(pos.x, pos.y),
+                    map.xy_to_idx(player_pos.x, player_pos.y),
+                    &*map,
+                );
+                if path.success && path.steps.len() > 1 {
+                    pos.x = path.steps[1] as i32 % map.width;
+                    pos.y = path.steps[1] as i32 / map.width;
+                    viewshed.dirty = true;
+                }
             }
         }
     }
